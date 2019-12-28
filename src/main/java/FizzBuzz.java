f9865f8f430280ed0dc85cd6e9252f3255752ccc
xuxuechaoxxc <1209939784@qq.com>
2019-12-28 12:48:04 +0800

    ref

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,15 +5,23 @@ public class FizzBuzz {
         if (i % 3 == 0 && i % 5 == 0) {
             return "FizzBuzz";
         }
+        String result;
         if (i % 3 == 0) {
             return "Fizz";
-        }
-        if (i % 5 == 0) {
-            return "Buzz";
-        }else {
-            return null;
+            if (i % 5 == 0) {
+                result = "FizzBuzz";
+            } else {
+                result = "Fizz";
+            }
+            return result;
         }
 
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
+        return i + "";
     }
+
+
 
 }

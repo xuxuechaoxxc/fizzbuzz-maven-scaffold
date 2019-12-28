f4480a79e771f45576fad05090d44c245aa6060d
xuxuechaoxxc <1209939784@qq.com>
2019-12-28 12:55:41 +0800

    ref

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,21 +3,25 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    private FizzBuzz fizzBuzz;
 
     @Test
     public void test1() {
-        String result = fizzBuzz.exc(3);
-        assertThat(result).isEqualTo("Fizz");
-    }
-    @Test
-    public void test2() {
-        String result = fizzBuzz.exc(5);
-        assertThat(result).isEqualTo("Buzz");
+        String result = FizzBuzz.exc(3);
+        assertThat(result).isEqualTo("1");
     }
     @Test
     public void test3() {
-        String result = fizzBuzz.exc(15);
+        String result = FizzBuzz.exc(3);
+        assertThat(result).isEqualTo("Fizz");
+    }
+    @Test
+    public void test5() {
+        String result = FizzBuzz.exc(5);
+        assertThat(result).isEqualTo("Buzz");
+    }
+    @Test
+    public void test15() {
+        String result = FizzBuzz.exc(15);
         assertThat(result).isEqualTo("FizzBuzz");
     }
 

354a2c5e1ffa757aa984d19ad63fef3992f67d30
xuxuechaoxxc <1209939784@qq.com>
2019-12-28 12:52:24 +0800

    ref

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,13 +1,8 @@
 public class FizzBuzz {
 
     public static String exc(int i) {
-
-        if (i % 3 == 0 && i % 5 == 0) {
-            return "FizzBuzz";
-        }
         String result;
         if (i % 3 == 0) {
-            return "Fizz";
             if (i % 5 == 0) {
                 result = "FizzBuzz";
             } else {
